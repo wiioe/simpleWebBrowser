8545ce2b204ffdafe4d010fe020b95f8698779fa
HyeonJu Jeong <0106grace@naver.com>
2016-06-02 20:56:57 +0900

    #2 Event Programming

--- app/src/main/java/kr/hs/e_mirim/grace0106/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/grace0106/simplewebbrowser/MainActivity.java
@@ -3,7 +3,9 @@ package kr.hs.e_mirim.grace0106.simplewebbrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,10 +23,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         butPre = (Button)findViewById(R.id.but_pre);
         web = (WebView)findViewById(R.id.web);
         butMove.setOnClickListener(this);
+        butPre.setOnClickListener(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webset = web.getSettings();
+        webset.setBuiltInZoomControls(true);
+        webset.setJavaScriptEnabled(true);
+        web.loadUrl("http://www.naver.com");
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId())
+        {
+            case R.id.but_move:
+                if (editUrl.getText().toString().contains("http://")==true)
+                {
+                    web.loadUrl(editUrl.getText().toString());
+                    break;
+                } else {
+                    web.loadUrl("http://" + editUrl.getText().toString());
+                    break;
+                }
+            case R.id.but_pre:
+                web.goBack();
+                break;
+        }
     }
 }
